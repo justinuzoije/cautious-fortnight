f96ec7d3d9e3a2002d5e49a072cab3fa53776092
Justin Uzoije <justin.uzoije@searchdiscovery.com>
2019-07-19 13:19:09 -0400

    Add Adobe Analytics server side

---
+++ adobe.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+</head>
+<body>
+Order Confirmation<br>
+Thanks for your order #0123456.
+<img src="https://102.112.207.net/b/ss/suite1,suite2/1/G.4--NS
+<codeph outputclass="syntax">
+  /123456?pageName=Order%20 Confirmation&events=purchase%2Cevent1&c1=Registered&purchaseID=0123456&products=Books%3BBook%20Name%3B1%3B19.95&state=CA&zip=90210&g=https%3A//www.somesite.com/cart/confirmation.asp" width="1" height="1" border="0" />
+ </body>
+ </html>
+
+</codeph outputclass="syntax">
